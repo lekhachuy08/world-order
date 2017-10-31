949b1bd7321dea31d475ad2f225845c85b1f5142
Huy Lê <lekhachuy08@gmail.com>
2017-10-31 18:24:30 +1100

    Add option choosing logic

--- World order/ViewController.swift
+++ World order/ViewController.swift
@@ -23,6 +23,7 @@ class Round {
 class ViewController: UIViewController {
     let data: Round = Round(question: "Who is Huy?", options: ["Huy", "Quang", "Thinh", "Anh"], answer: 0)
     
+    @IBOutlet weak var question: UITextView!
     @IBOutlet weak var option1: UIButton!
     @IBOutlet weak var option2: UIButton!
     @IBOutlet weak var option3: UIButton!
@@ -30,8 +31,69 @@ class ViewController: UIViewController {
     @IBOutlet weak var option5: UIButton!
     @IBOutlet weak var option6: UIButton!
     
+    var options = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        options.append(option1)
+        options.append(option2)
+        options.append(option3)
+        options.append(option4)
+        options.append(option5)
+        options.append(option6)
+        
+        question.text = data.question
+        question.isUserInteractionEnabled = false
+        question.font = UIFont(name: (question.font?.fontName)!, size: UIScreen.main.bounds.width / 8)
+        
+        for i in 0...5 {
+            if (i < 4) {
+                options[i].setTitle(data.options[i], for: UIControlState.normal)
+            } else {
+                options[i].setTitle("", for: UIControlState.normal)
+            }
+            options[i].titleLabel?.font = UIFont(name: (question.font?.fontName)!, size: UIScreen.main.bounds.width / 16)
+        }
+    }
+    
+    func press(_ option: Int) -> Bool {
+        options[data.answer].setTitleColor(UIColor.green, for: UIControlState.normal)
+        
+        for i in 0...5 {
+            options[i].isUserInteractionEnabled = false
+        }
+        
+        if (option == data.answer) {
+            return true
+        } else {
+            options[option].setTitleColor(UIColor.red, for: UIControlState.normal)
+            return false
+        }
+    }
+    
+    @IBAction func pressButton1(_ sender: Any) {
+        press(0)
+    }
+    
+    @IBAction func pressButton2(_ sender: Any) {
+        press(1)
+    }
+    
+    @IBAction func pressButton3(_ sender: Any) {
+        press(2)
+    }
+    
+    @IBAction func pressButton4(_ sender: Any) {
+        press(3)
+    }
+    
+    @IBAction func pressButton5(_ sender: Any) {
+        press(4)
+    }
+    
+    @IBAction func pressButton6(_ sender: Any) {
+        press(5)
     }
     
     override func didReceiveMemoryWarning() {
